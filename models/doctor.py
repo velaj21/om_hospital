d6a1b326f954e678f1f11c624a8880bc1e30a02e
user <lejva.velaj1@gmail.com>
2024-05-10 14:21:14 +0200

    dfs

--- models/doctor.py
+++ models/doctor.py
@@ -10,9 +10,8 @@ class HospitalDoctor(models.Model):
     _rec_name = 'doctor_name'
 
     # todo beje nje tabel te re per ratings qe i jep doktorrit dhe gjej mesataren e tij
-    rateDoctorin = fields.Integer(
-        string='RateDoctorin',
-        required=False)
+    average_rating = fields.Float(string='Average Rating', compute='_compute_average_rating', store=True)
+
     # todo vendos email dhe nr telefoni bej valid vtm per shqiperin
     doctor_name = fields.Char(string='Name', required=True, tracking=True)
     age = fields.Integer(string='Age', tracking=True, copy=False)
@@ -25,6 +24,7 @@ class HospitalDoctor(models.Model):
     image = fields.Binary(string="Patient Image")
     appointment_count = fields.Integer(string='Appointment Count', compute='_compute_appointment_count')
     active = fields.Boolean(string="Active", default=True)
+    rating_ids = fields.One2many('hospital.rating', 'doctor_id', string='Ratings')
 
     def copy(self, default=None):
         if default is None:
@@ -38,6 +38,14 @@ class HospitalDoctor(models.Model):
         for rec in self:
             appointment_count = self.env['hospital.appointment'].search_count([('doctor_id', '=', rec.id)])
             rec.appointment_count = appointment_count
+
+    def _compute_average_rating(self):
+        for rec in self:
+            total_rating = sum(rec.rating_ids.mapped('rating'))
+            if rec.rating_ids:
+                rec.average_rating = total_rating / len(rec.rating_ids)
+            else:
+                rec.average_rating = 0.0
 
 
 # todo krijo nje model te ri turni vendose me 3 psh
