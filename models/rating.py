5957f7428134f5cee05144ff693f78f3c71c970e
user <lejva.velaj1@gmail.com>
2024-05-10 13:03:23 +0200

    dfs

--- models/rating.py
+++ models/rating.py
@@ -5,5 +5,12 @@ class Rating(models.Model):
     _name = 'hospital.rating'
 
     doctor_id = fields.Many2one(comodel_name='hospital.doctor', string='Doctor_id')
-    rating = fields.Float(string='Rating', required=True)
+    rating = fields.Selection([
+        ('0', '0'),
+        ('1', '1'),
+        ('2', '2'),
+        ('3', '3'),
+        ('4', '4'),
+        ('5', '5')
+    ], string='Rating', required=True)
     comment = fields.Text(string='Comment')
