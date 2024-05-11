de7bd8fe9d02912253672307c8d30939047b2300
user <lejva.velaj1@gmail.com>
2024-05-11 11:46:17 +0200

    time

---
+++ models/shift.py
@@ -0,0 +1,9 @@
+from odoo import fields, models, api
+class $ModelName$ (models.Model):
+    _name = '$ProjectName$.$TableName$'
+    _description = '$Description$'
+
+    name = fields.Char()
+    $END$
+
+
