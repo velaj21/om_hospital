574cedf44e778a6ba523429af4521a3bb582b95a
lejva.velaj1@gmail.com <lejva.velaj1@gmail.com>
2024-05-15 12:26:56 +0200

    shift added selection

--- models/shift.py
+++ models/shift.py
@@ -6,12 +6,33 @@ class Shift(models.Model):
     _description = 'Description'
 
     doctor_id = fields.Many2one(comodel_name='hospital.doctor', string='Doctor', required=True)
-    TIME_SLOTS = [
-        ('08:00', '08:00'),
-        ('09:00', '09:00'),
-        ('10:00', '10:00'),
-        # Add more time slots as needed
+
+    TIME_SLOTS_AM_PM = [
+        ('00:00', '12:00 AM'),
+        ('01:00', '01:00 AM'),
+        ('02:00', '02:00 AM'),
+        ('03:00', '03:00 AM'),
+        ('04:00', '04:00 AM'),
+        ('05:00', '05:00 AM'),
+        ('06:00', '06:00 AM'),
+        ('07:00', '07:00 AM'),
+        ('08:00', '08:00 AM'),
+        ('09:00', '09:00 AM'),
+        ('10:00', '10:00 AM'),
+        ('11:00', '11:00 AM'),
+        ('12:00', '12:00 PM'),
+        ('13:00', '01:00 PM'),
+        ('14:00', '02:00 PM'),
+        ('15:00', '03:00 PM'),
+        ('16:00', '04:00 PM'),
+        ('17:00', '05:00 PM'),
+        ('18:00', '06:00 PM'),
+        ('19:00', '07:00 PM'),
+        ('20:00', '08:00 PM'),
+        ('21:00', '09:00 PM'),
+        ('22:00', '10:00 PM'),
+        ('23:00', '11:00 PM'),
     ]
 
-    start_time = fields.Selection(TIME_SLOTS, string='Start Time')
-    end_time = fields.Selection(TIME_SLOTS, string='End Time')
+    start_time = fields.Selection(TIME_SLOTS_AM_PM, string='Start Time')
+    end_time = fields.Selection(TIME_SLOTS_AM_PM, string='End Time')
