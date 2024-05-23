375da6229aa0c1d611affea5248cb044e5fed547
lejva.velaj1@gmail.com <lejva.velaj1@gmail.com>
2024-05-23 14:46:57 +0200

    errore

--- wizard/validate_xml.py
+++ wizard/validate_xml.py
@@ -1,4 +1,4 @@
-from xlrd.xlsx import ET
+import xml.etree.ElementTree as ET
 import re
 
 from odoo import models, fields, api
@@ -15,21 +15,30 @@ class MyWizard(models.TransientModel):
     def validate_xml(self):
         file_content = base64.b64decode(self.file).decode('utf-8')
 
-        root = ET.fromString(file_content)
+        root = ET.fromstring(file_content)
 
         # Find SendDateTime attribute
-        send_datetime = root.find('.//{http://example.com/ns3}Header').attrib.get('SendDateTime')
+        header_element = root.find('.//{http://schemas.xmlsoap.org/soap/envelope/}Header')
+        if header_element is not None:
+            send_datetime = header_element.attrib.get('SendDateTime')
 
-        # Check if SendDateTime exists and has correct format
-        if send_datetime and len(send_datetime) == 36 and re.match(
-                r'^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[1-5][0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$',
-                send_datetime):
-            # Set is_valid field to True if validation succeeds
-            self.is_valid = True
-        else:
-            # Set is_valid field to False if validation fails
-            self.is_valid = False
+            # Check if SendDateTime exists and has correct format
+            if send_datetime and len(send_datetime) == 36 and re.match(
+                    r'^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[1-5][0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$',
+                    send_datetime.strip()):
+                # Set is_valid field to True if validation succeeds
+                self.is_valid = True
+                return {
+                    'type': 'ir.actions.act_window',
+                    'res_model': self._name,
+                    'res_id': self.id,
+                    'view_type': 'form',
+                    'view_mode': 'form',
+                    'target': 'new',
+                }
 
+        # Set is_valid field to False if validation fails
+        self.is_valid = False
         return {
             'type': 'ir.actions.act_window',
             'res_model': self._name,
